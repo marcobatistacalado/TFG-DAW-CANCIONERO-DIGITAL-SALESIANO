7cafde9a8a0c0d82954a6fc07ea139333398521d
Marco Batista Calado <165885717+marcobatistacalado@users.noreply.github.com>
2025-03-31 20:12:09 +0200

    terminada la migracion

--- cancionero_digital_salesiano/cancionero_digital_salesiano/settings.py
+++ cancionero_digital_salesiano/cancionero_digital_salesiano/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'cancionero_digital_salesiano.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cancionero_db',  # Nombre correcto de tu base de datos
+        'USER': 'programador',  # Nombre del usuario
+        'PASSWORD': 'tfg.programador.25',  # Contraseña correcta
+        'HOST': 'localhost',  # Si estás en local
+        'PORT': '5432',  # Puerto por defecto
+    }
 }
+
 
 
 # Password validation
